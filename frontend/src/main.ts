c9157f6f9222edf2f93302f08f634563414b46a5
ZhengFang121 <huangzf.jp@gmail.com>
2026-08-03 15:48:02 +0800

    chore: 建立前端開發環境

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+import router from './router'
+
+import 'primeicons/primeicons.css'
 import './style.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.use(router)
+app.use(pinia)
+
+app.mount('#app')
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>跑者菲迪 Pheidi the Runner</h1>
+    <p>Frontend 環境建立完成！</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
